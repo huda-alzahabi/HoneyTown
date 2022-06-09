e52e092b7d09ee0023dc33f29a3b3df1e8a2a1ee
huda-alzahabi <hudazahabi00@gmail.com>
2022-06-09 19:21:59 +0300

    Implemeted JWT Authentication and created the database tables

--- database/migrations/2014_10_12_000000_create_items_table.php
+++ database/migrations/2014_10_12_000000_create_items_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateItemsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('items', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('price');
+            $table->string('image');
             $table->timestamps();
         });
     }
