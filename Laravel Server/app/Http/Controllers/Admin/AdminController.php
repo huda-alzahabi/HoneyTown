08c89cb8ebba28fdf87e93216e799294b8cd5e08
huda-alzahabi <hudazahabi00@gmail.com>
2022-06-09 22:55:40 +0300

    Implemented add item functionality

--- Laravel Server/app/Http/Controllers/Admin/AdminController.php
+++ Laravel Server/app/Http/Controllers/Admin/AdminController.php
@@ -4,8 +4,20 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Item;
 
 class AdminController extends Controller
 {
-    //
+    public function addItem(Request $request){
+        $item = new Item;
+        $item->name = $request->name;
+        $item->price = $request->price;
+        $item->image= $request->image;
+        $item->save();
+
+        return response()->json([
+            "status" => "Success",
+            "item" => $item
+        ], 200);
+    }
 }
--- Laravel Server/routes/api.php
+++ Laravel Server/routes/api.php
@@ -6,11 +6,23 @@ use App\Http\Controllers\User\UserController;
 use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\JWTController;
 
-Route::group(['middleware' => 'api'], function($router) {
-    Route::post('/register', [JWTController::class, 'register']);
-    Route::post('/login', [JWTController::class, 'login']);
-    Route::post('/logout', [JWTController::class, 'logout']);
-    Route::post('/refresh', [JWTController::class, 'refresh']);
-    Route::post('/profile', [JWTController::class, 'profile']);
+Route::group(['prefix' => 'v1'], function(){
+    Route::group(['prefix' => 'user'], function(){
+        Route::get('/all_users/{id?}', [UserController::class, 'getAllUsers']);
+        Route::post('/register/{user_type_id}', [UserController::class, 'signUp']);
+    });
 
+    Route::group(['prefix' => 'admin'], function(){
+        // Route::get('/restaurants/{id?}', [AdminController::class, 'getAllRestos']);
+        // Route::get('/search/{name}', [RestoController::class, 'getRestoByName']);
+        Route::post('/add_item', [AdminController::class, 'addItem']);
+    });
+
+    Route::group(['middleware' => 'api'], function($router) {
+        Route::post('/register', [JWTController::class, 'register']);
+        Route::post('/login', [JWTController::class, 'login']);
+        Route::post('/logout', [JWTController::class, 'logout']);
+        Route::post('/refresh', [JWTController::class, 'refresh']);
+        Route::post('/profile', [JWTController::class, 'profile']);
+    });
 });
