3e253a5b9d3169e0553cf829c99c90694729973e
huda-alzahabi <hudazahabi00@gmail.com>
2022-06-11 23:03:36 +0300

    Added functions to create new category and new item

--- FrontEnd User/scripts/apis.js
+++ FrontEnd User/scripts/apis.js
@@ -4,3 +4,12 @@ function login() {
     };
 }
 login();
+
+function createCategory(id, name) {
+    let category_name = document.createElement("h2");
+    category_name.setAttribute("id", id);
+    category_name.innerHTML = name;
+
+    let category = document.getElementsByClassName("category-names")[0];
+    category.appendChild(category_name);
+}
