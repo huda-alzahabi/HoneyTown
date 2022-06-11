6ff4d0cd01d20e0e98c1afeba8f2c8b8f7d2dbb3
huda-alzahabi <hudazahabi00@gmail.com>
2022-06-11 23:22:43 +0300

    Deleted images column from categories table

--- Laravel Server/database/migrations/2014_10_12_000000_remove_image_column.php
+++ Laravel Server/database/migrations/2014_10_12_000000_remove_image_column.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class UpdateCategoriesTable extends Migration
+class RemoveImageColumn extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,7 @@ class UpdateCategoriesTable extends Migration
     public function up()
     {
         Schema::table('categories', function (Blueprint $table) {
-            $table->string('image')->after('name');
+            $table->dropColumn('image');
         });
     }
 
