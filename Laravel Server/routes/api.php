2b2ad8e4c20c1c5608003ebccd994b44415af7b2
huda-alzahabi <hudazahabi00@gmail.com>
2022-06-13 00:05:08 +0300

    removed get all items method

--- Laravel Server/routes/api.php
+++ Laravel Server/routes/api.php
@@ -8,21 +8,20 @@ use App\Http\Controllers\JWTController;
 
 Route::group(['prefix' => 'v1'], function(){
     Route::group(['prefix' => 'user'], function(){
-       Route::get('/all_items', [UserController::class, 'getAllItems']);
-       Route::get('/favorites', [UserController::class, 'getFavoriteItems']);
-       Route::post('/items_by_category', [UserController::class, 'getItemsByCategory']);
+    Route::get('/all_categories', [UserController::class, 'getAllCategories']);
+       Route::get('/items_by_category', [UserController::class, 'getItemsByCategory']);
        Route::group(['middleware' => 'role.favorites'], function(){
            Route::post('/add_to_favorites', [UserController::class, 'addToFavorites']);
+           Route::get('/favorites', [UserController::class, 'getFavoriteItems']);
         });
 
     });
 
     Route::group(['prefix' => 'admin'], function(){
-        Route::get('/all_categories', [AdminController::class, 'getAllCategories']);
-       Route::group(['middleware' => 'role.admin'], function(){
+    //   Route::group(['middleware' => 'role.admin'], function(){
             Route::post('/add_category', [AdminController::class, 'addCategory']);
             Route::post('/add_item', [AdminController::class, 'addItem']);
-       });
+     //  });
     });
 
     Route::group(['middleware' => 'api'], function($router) {
