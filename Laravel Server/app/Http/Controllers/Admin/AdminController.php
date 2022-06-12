473064ccdd8bb4cbced745d5a8078b9928242a89
huda-alzahabi <hudazahabi00@gmail.com>
2022-06-12 16:27:04 +0300

    Implemented add item functionality with image

--- Laravel Server/app/Http/Controllers/Admin/AdminController.php
+++ Laravel Server/app/Http/Controllers/Admin/AdminController.php
@@ -11,10 +11,18 @@ use App\Models\Category;
 class AdminController extends Controller
 {
     public function addItem(Request $request){
+         $this->validate($request,[
+        'name' => 'required|max:250',
+        'price' => 'required',
+        'image' => 'required|file|mimes:jpeg,png,jpg,gif,svg',
+        'categories_id' => 'required',  ]);
+
+        $path = $request->file('image')->store('public/image/image');
         $item = new Item;
         $item->name = $request->name;
         $item->price = $request->price;
-        $item->image= $request->image;
+        $item->image= $path;
+        $item->categories_id= $request->categories_id;
         $item->save();
 
         return response()->json([
@@ -26,12 +34,18 @@ class AdminController extends Controller
     public function addCategory(Request $request){
         $category = new Category;
         $category->name = $request->name;
-        $category->image= $request->image;
         $category->save();
 
         return response()->json([
             "status" => "Success",
             "category" => $category
+        ], 200);
+    }
+     public function getAllCategories(){
+       $categories = Category::all();
+        return response()->json([
+            "status" => "Success",
+            "categories" => $categories
         ], 200);
     }
 
